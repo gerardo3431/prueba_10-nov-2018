8ed6ba86081c58555ce467562810e80b77b697a3
Gerardo Olán <gerardo_olan@hotmail.com>
2018-11-10 23:51:41 -0600

    averigua el cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ¿Qui iciste Manchiiiiii? :v
+                    ¿kiubo JoJo? :v
                 </div>
 
                 <div class="links">
